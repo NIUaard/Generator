ac49271630c23136fcaaaf7fc4907b39400119c8
Philipe Piot <piot@nicadd.niu.edu>
2019-10-28 08:46:52 -0500

    updated example with iso

--- gen_r_unit_t_gaus_p_iso.py
+++ gen_r_unit_t_gaus_p_iso.py
@@ -12,10 +12,10 @@ import generatorTool as gt
 
 # set initial parameters
 
-N       = 1000   # number of macroparticles
+N       = 10000   # number of macroparticles
 plot_it = 1 
 # all space/time unit should be SI:
-cathode_spot_radius = 2e-3
+cathode_spot_radius = 2e-5  # 2e-3
 sigma_t_laser       = 3.4e-12 
 laser_sigma_cut     = 3
 
@@ -27,7 +27,8 @@ Excess_kinetic_eV   = 0.75
 X, Y= gt.tran_rad_unif (N, cathode_spot_radius)
 T   = gt.gauss_1d_cut  (N, sigma_t_laser, laser_sigma_cut) 
 # cold = no thermal emittance
-PX, PY, PZ = gt.momt_cold (N, Excess_kinetic_eV) 
+# PX, PY, PZ = gt.momt_cold (N, Excess_kinetic_eV) 
+PX, PY, PZ = gt.momt_thermal_iso (N, Excess_kinetic_eV) 
 
 
 print(len(X), len(Y), len(T), len(PX), len(PY), len(PZ))
@@ -38,17 +39,19 @@ gt.dump_ImpactT_cathode(X,Y,T,PX,PY,PZ)
 
 if (plot_it==1):
    plt.subplot (2,2,1)
-   plt.plot (X,Y,'.')
+   plt.plot (X,Y,'.', markersize=0.2)
 
    plt.subplot (2,2,2)
-   plt.hist(T, 101, normed=1, lw=3, edgecolor='red', facecolor="None")
+   plt.plot (X,PX,'.', markersize=0.2)
+#   plt.hist(T, 101, normed=1, lw=3, edgecolor='red', facecolor="None")
 
    plt.subplot (2,2,3)
+   plt.plot (Y,PY,'.', markersize=0.2)
 #   plt.plot (PX,PY,'.')
-   plt.plot (X,PX,'.')
+#   plt.plot (X,PX,'.', markersize=0.2)
  
    plt.subplot (2,2,4)
-   plt.plot (T,PZ,'.')
+   plt.plot (T,PZ,'.', markersize=0.2)
 
    plt.show()
 
